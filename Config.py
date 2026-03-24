fde1d451b56dc67e4342fdbfb1a456549dff0231
wenzhong187-del <wenzhong187@gmail.com>
2026-03-24 20:34:09 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,81 +1,46 @@
 # -*- coding: utf-8 -*-
-# @Time    : 2021/6/19 2:44 下午
-# @Author  : Haonan Wang
-# @File    : Config.py
-# @Software: PyCharm
+"""
+Config for PLRQuant
+Focused on Pupillary Light Reflex (PLR) Video Segmentation and Analysis.
+"""
+
 import os
-import torch
 import time
-import ml_collections
+import argparse
 
-# 任务 ID 映射：0: Pupil(瞳孔), 1: OCT(断层), 2: OCTA(血流血管)
-# 请根据你当前训练的数据集手动修改这个值
-task_id = 1  # 比如你现在正在跑 OCTA 血管分割，就设为 2
+task_name = 'PLRQuant'
+model_name = 'PLRQuantNet'
+train_dataset = './datasets/PLR_Data/' 
+test_dataset  = './datasets/PLR_Data/' 
 
-# 损失函数权重分配
-dice_weight = 0.4
-BCE_weight = 0.4
-clDice_weight = 0.2 # 针对眼科图像结构的创新权重
+session_name = 'PLR_Session_' + time.strftime('%m.%d_%Hh%M')
+save_path    = os.path.join(task_name + '_results', model_name, session_name)
+model_path   = os.path.join(save_path, 'models/')
+log_path     = os.path.join(save_path, session_name + ".log")
+vis_path     = os.path.join(save_path, 'visualize_val/')
 
-## PARAMETERS OF THE MODEL
-save_model = True
-tensorboard = True
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-use_cuda = torch.cuda.is_available()
-seed = 666
-os.environ['PYTHONHASHSEED'] = str(seed)
-
-cosineLR = True # whether use cosineLR or not
-n_channels = 3
-n_labels = 7
-epochs = 500
-img_size = 224
-print_frequency = 10
-save_frequency = 10
-vis_frequency = 10
-early_stopping_patience = 50
-
-pretrain = False
-task_name = 'pupil' # GlaS MoNuSeg
-# task_name = 'GlaS'
-learning_rate = 3e-4
+for p in [model_path, vis_path]:
+    if not os.path.exists(p):
+        os.makedirs(p, exist_ok=True)
+epochs = 300
 batch_size = 4
+learning_rate = 1e-4
+early_stopping_patience = 40
+# 图像与时序设置
+img_size = 224
+seq_len = 5
+n_channels = 3
+n_labels = 1
+# 实验设置
+kfold = 5
+seed = 666
+cosineLR = True
+print_frequency = 10
+save_frequency = 50
+vis_frequency = 10
+tensorboard = True
+tensorboard_folder = os.path.join(save_path, 'tensorboard_logs/')
+pixel_to_mm_ratio = 0.05
+video_fps = 60
 
 
-
-model_name = 'PLRQuant_pretrain'
-
-train_dataset = './datasets/'+ task_name+ '/Train_Folder/'
-val_dataset = './datasets/'+ task_name+ '/Val_Folder/'
-test_dataset = './datasets/'+ task_name+ '/Test_Folder/'
-session_name       = 'Test_session' + '_' + time.strftime('%m.%d_%Hh%M')
-save_path          = task_name +'/'+ model_name +'/' + session_name + '/'
-model_path         = save_path + 'models/'
-tensorboard_folder = save_path + 'tensorboard_logs/'
-logger_path        = save_path + session_name + ".log"
-visualize_path     = save_path + 'visualize_val/'
-
-
-##########################################################################
-# CTrans configs
-##########################################################################
-def get_CTranS_config():
-    config = ml_collections.ConfigDict()
-    config.transformer = ml_collections.ConfigDict()
-    config.KV_size = 960  # KV_size = Q1 + Q2 + Q3 + Q4
-    config.transformer.num_heads  = 4
-    config.transformer.num_layers = 4
-    config.expand_ratio           = 4  # MLP channel dimension expand ratio
-    config.transformer.embeddings_dropout_rate = 0.1
-    config.transformer.attention_dropout_rate = 0.1
-    config.transformer.dropout_rate = 0
-    config.patch_sizes = [16,8,4,2]
-    config.base_channel = 64 # base channel of U-Net
-    config.n_classes = 1
-    return config
-
-
-
-
-# used in testing phase, copy the session name in training phase
-test_session = "Test_session_03.05_20h01"
